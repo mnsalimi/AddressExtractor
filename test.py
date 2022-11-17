740a19d3d2ce0fd581dedb743a18e0c573ff0359
msalimi <sartakhti.salimi@gmail.com>
2022-11-17 18:35:41 +0330

    testcases added

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from address_extractor.address_extractor import AddressExtractor
 
 if __name__ == "__main__":
     extractor = AddressExtractor()
-    address = extractor.run("انگلیس خیابان ملکی") 
+    address = extractor.run("جاده قم") 
     print(address)
